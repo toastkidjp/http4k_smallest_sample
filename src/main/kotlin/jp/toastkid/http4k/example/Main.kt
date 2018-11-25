ad5b58d8fbd0f77f30ce602ebeba28db285ae3ce
Toast Kid <super_q1m@yahoo.co.jp>
2018-11-25 19:20:13 +0900

    Initial commit.

---
+++ src/main/kotlin/jp/toastkid/http4k/example/Main.kt
@@ -0,0 +1,16 @@
+package jp.toastkid.http4k.example
+
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+
+fun main(args: Array<String>) {
+    routes("/hello" bind Method.GET to { _: Request -> Response(OK).body("hello!!") })
+            .asServer(SunHttp(8000))
+            .start()
+}
